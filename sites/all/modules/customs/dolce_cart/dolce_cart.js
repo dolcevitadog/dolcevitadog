1b60e65739f955873370f9ca5b21622e37398d02
dolcevitadog <contact@toutpourmonchien.fr>
2017-05-25 20:34:18 +0200

    Product and Cart Features for theming

--- sites/all/modules/customs/dolce_cart/dolce_cart.js
+++ sites/all/modules/customs/dolce_cart/dolce_cart.js
@@ -3,5 +3,17 @@
 
     $(document).ready(function() {
         $('#edit-quantity').spinner();
+        var readmore = Drupal.t('read more');
+        var readless = Drupal.t('read less');
+        $('.node-product-type .field-name-body .field-item').expander({
+            slicePoint: 300,
+            expandPrefix: '...<br />',
+            expandText: readmore,
+            userCollapseText: readless,
+            expandEffect: 'fadeIn',
+            expandSpeed: 250,
+            collapseEffect: 'fadeOut',
+            collapseSpeed: 200
+        });
     });
  })(jQuery);
--- sites/all/themes/dolce/templates/product/node--product--type.tpl.php
+++ sites/all/themes/dolce/templates/product/node--product--type.tpl.php
@@ -10,8 +10,9 @@
     <?php if ($display_submitted): ?>
         <footer class="submitted"><?php print $date; ?> -- <?php print $name; ?></footer>
     <?php endif; ?>
-    <div class="col-lg-6 col-sm-6 col-xs-12 col-md-6 clearfix">
+    <div class="col-lg-6 col-sm-6 col-xs-12 col-md-6">
         <?php print render($content['product:field_images']); ?>
+        <?php print render($content['field_gallery']); ?>
     </div>
     <div class="col-lg-6 col-sm-6 col-xs-12 col-md-6">
         <div<?php print $content_attributes; ?>>
@@ -19,11 +20,13 @@
             // We hide the comments and links now so that we can render them later.
             hide($content['comments']);
             hide($content['links']);
+            hide($content['field_gallery']);
             print render($content);
+
             ?>
         </div>
     </div>
-    <div class="container-24 grid-24 clearfix">
+    <div class="col-lg-12 col-sm-12 col-xs-12 col-md-12 clearfix">
         <?php if (!empty($content['links'])): ?>
             <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
         <?php endif; ?>
