15598ac7fef92b5666bb84fd9d11481e0525ca78
dolcevitadog <contact@toutpourmonchien.fr>
2020-03-30 10:24:28 +0200

    Updat DolceCart Module

---
+++ sites/all/modules/customs/dolce_cart/dolce_cart_shopping_cart.tpl.php
@@ -0,0 +1,8 @@
+<div id="dolce-cart">
+    <a href="/cart">
+        <span class="dolce-cart-bg"></span>
+        <span class="dolce-cart-quantity"><?php print $cart['quantity'];?></span>
+        <span class="dolce-cart-price"><?php print $cart['price']; ?></span>
+        <span class="dolce-cart-label nomobile"><?php print t('My cart'); ?></span>
+    </a>
+</div>
