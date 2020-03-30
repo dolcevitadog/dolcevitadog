2ba117b804a626fa4017cdbdd530f31d30972bb5
dolcevitadog <contact@toutpourmonchien.fr>
2020-03-30 10:24:42 +0200

    New module Dolce User Links

---
+++ sites/all/modules/customs/dolce_user_links/dolce_user_links.tpl.php
@@ -0,0 +1,6 @@
+<div id="dolce-user-links-block">
+    <a href="/<?php print $links['href'];?>">
+            <span class="dolce-user-links <?php print $links['class'];?>"></span>
+            <span class="dolce-user-links-title nomobile"><?php print $links['title'];?></span>
+    </a>
+</div>
