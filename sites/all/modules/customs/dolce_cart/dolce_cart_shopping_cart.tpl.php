4e0124675177b941888e048d28bba3b3f5f174d4
dolcevitadog <contact@toutpourmonchien.fr>
2020-04-03 17:08:16 +0200

    Update templates for Dolce cart and Dolce User links

--- sites/all/modules/customs/dolce_cart/dolce_cart_shopping_cart.tpl.php
+++ sites/all/modules/customs/dolce_cart/dolce_cart_shopping_cart.tpl.php
@@ -2,7 +2,7 @@
     <a href="/cart">
         <span class="dolce-cart-bg"></span>
         <span class="dolce-cart-quantity"><?php print $cart['quantity'];?></span>
-        <span class="dolce-cart-price"><?php print $cart['price']; ?></span>
-        <span class="dolce-cart-label nomobile"><?php print t('My cart'); ?></span>
+        <span class="dolce-cart-price hidden-xs"><?php print $cart['price']; ?></span>
+        <span class="dolce-cart-label nomobile hidden-xs"><?php print t('My cart'); ?></span>
     </a>
 </div>
