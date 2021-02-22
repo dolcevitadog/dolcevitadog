80bbfd10db445c96ebaaef99e0b802d768adbd3e
dolcevitadog <contact@toutpourmonchien.fr>
2021-02-22 14:47:40 +0100

    Replay last modification

--- sites/all/modules/customs/dolce_event/dolce_event_replay.tpl.php
+++ sites/all/modules/customs/dolce_event/dolce_event_replay.tpl.php
@@ -105,7 +105,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title"><?php echo t('Advertising'); ?></h4>
+            <h4 class="modal-title"><?php echo t('Important'); ?></h4>
           </div>
           <div class="modal-body">
             <?php print $data['disclaimer']; ?>
