6ce1af6026c478ea166f6475b8815df20288756a
dolcevitadog <contact@toutpourmonchien.fr>
2021-05-19 15:04:55 +0200

    Add snack suggested products

---
+++ sites/all/themes/dolce/templates/view/views-view-fields--snack.tpl.php
@@ -0,0 +1,12 @@
+<div class="snack-product-wrapper col-lg-12 col-sm-12 col-xs-12 col-md-12">
+    <?php foreach ($fields as $id => $field): ?>
+        <?php if (!empty($field->separator)): ?>
+            <?php print $field->separator; ?>
+        <?php endif; ?>
+
+        <?php print $field->wrapper_prefix; ?>
+        <?php print $field->label_html; ?>
+        <?php print $field->content; ?>
+        <?php print $field->wrapper_suffix; ?>
+    <?php endforeach; ?>
+</div>
